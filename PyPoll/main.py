5f6afcccb677e27534ccb2937586a57103f89733
toluwase adurogbola <5t4david@gmail.com>
2021-04-13 08:57:25 -0500

    csv data added to pd dataframe

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,9 +13,15 @@ with open(csvpath) as csvfile:
     Total_Profit = []
 
     for row in csvreader:
-        print (row)
+        #print (row)
         Total_Row += 1
     print("Election Results")
     print("--------------------------")
     print("Total Votes: " + str(Total_Row))
     print("--------------------------")
+with open(csvpath) as csvfile:
+    csvreader2 = csv.reader(csvfile, delimiter=',')
+    for row in csvreader2:
+        Polls = pd.DataFrame(csvreader2, columns= ['Voter ID','County','Candidate'])
+        Candidates_Count = Polls["Candidate"].value_counts()
+    #print(Candidates_Count)
